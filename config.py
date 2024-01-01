27579adc483aec1b45205fa2129568abd94b7f9a
Mohit xD <147937463+AxZeRxD@users.noreply.github.com>
2024-01-01 22:37:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,6 @@
         "channels_name": [
             "AIZER X VIRUS ",
             "The Fucking Kimd Here",
-            "HACKING R1DERS",
             "AIZER X VIRUS BAP HAI"
         ],
         "roles_name": [
@@ -14,9 +13,7 @@
             "Aizer x virus  Papa Here"
         ],
         "messages_content": [
-            "@everyone @here **Aizer x Virus  Papa Aye The** https://discord.gg/ntontop",
-            "@everyone @here **Aizer x Virus  Papa Aye The** https://discord.gg/ntontop",
-            "@everyone @here **Aizer x Virus  Papa Aye The** https://discord.gg/ntontop"
+            "@everyone @here https://discord.gg/abusers",
         ]
     }
 }
